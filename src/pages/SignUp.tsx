09a71a5f2cebe35bbaadad42165bb33d26c2a0b3
LeeYujeong <yujlee242@gmail.com>
2023-01-21 21:29:01 +0900

    [feat] SignUp 컴포넌트 UI, 링크 이동 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  AuthButton,
+  AuthLogo,
+  AuthLogoImg,
+  AuthText,
+  AuthWrapper,
+  Background,
+  Input,
+  InputWrapper,
+  Label,
+  LinkText,
+  Title,
+} from './Login';
 
 export default function SignUp() {
-    return (
-        <div>
-            SignUp
-        </div>
-    );
+  return (
+    <Background>
+      <AuthWrapper>
+        <AuthLogo>
+          <Link to="/">
+            <AuthLogoImg src="/assets/logo.png" />
+          </Link>
+        </AuthLogo>
+        <Title>회원가입</Title>
+        <form>
+          <InputWrapper>
+            <Label htmlFor="email">이메일</Label>
+            <Input type="text" id="email" placeholder="이메일을 입력해주세요" />
+          </InputWrapper>
+          <InputWrapper>
+            <Label htmlFor="password">비밀번호</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="비밀번호를 입력해주세요"
+            />
+          </InputWrapper>
+          <AuthButton type="button">가입하기</AuthButton>
+          <AuthText>
+            계정이 있으신가요?
+            <Link to={'/login'}>
+              <LinkText>로그인</LinkText>
+            </Link>
+          </AuthText>
+        </form>
+      </AuthWrapper>
+    </Background>
+  );
 }
-
