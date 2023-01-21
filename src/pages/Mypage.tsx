c6c7e2762afd4ec1a74a709770b2f3d7b690bed2
LeeYujeong <yujlee242@gmail.com>
2023-01-21 21:29:47 +0900

    [feat] MyPage 클릭 시 로그인 여부 검증 추가

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { authService } from '../common/firebase';
 
 export default function Mypage() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (!authService.currentUser) {
+      alert('로그인이 필요합니다.');
+      return navigate('/login', { state: pathname });
+    }
+  }, []);
+
   return <div>Mypage</div>;
 }
