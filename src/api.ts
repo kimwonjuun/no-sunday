d3c766ca3f2b5f5baf6f363d5d87f103db01690f
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-01-25 10:54:30 +0900

    [feat] 메인페이지 내비게이트 추가 및 UI 수정

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 export const request = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
+<<<<<<< Updated upstream
     key: 'AIzaSyD79zpSL8ngB3-xRtASNN8eYNV-GjELUjI',
     // 원준님 키
     // key: 'AIzaSyBg_6kb8jCXeEsFpT0X3m9qkoW4s9Dzl3Y',
@@ -11,6 +12,12 @@ export const request = axios.create({
     // key: 'AIzaSyCBDEEW6FlMFSngjrSRM4H56y1fMYX6y9M',
     // 진양님 키
     // key: 'AIzaSyDtU_PEu_8kJk4zoY_AuflBPiiYx5BtZZw',
+=======
+    // key: 'AIzaSyBg_6kb8jCXeEsFpT0X3m9qkoW4s9Dzl3Y', // 김원준 key
+    key: 'AIzaSyCU6tOYP8q0b4SxUfo-vlH-E3P0bGYRo4U', // 김원준 key2
+    // key: 'AIzaSyCBDEEW6FlMFSngjrSRM4H56y1fMYX6y9M', // 유정님 key
+    // key: 'AIzaSyDtU_PEu_8kJk4zoY_AuflBPiiYx5BtZZw', // 진양님 key
+>>>>>>> Stashed changes
     // 키 환경변수
     // key: process.env.REACT_APP_YOUTUBE_API_KEY,
   },
