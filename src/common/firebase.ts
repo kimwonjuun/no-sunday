69da6f236ebd39842a76835fad2e6fc8f3c01823
Jinyang-park <dbsskdud60@gmail.com>
2023-02-02 13:58:20 +0900

    뭐야?

--- src/common/firebase.ts
+++ src/common/firebase.ts
@@ -2,17 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBvaVpp63pkk3Q_nbYddC3DQkWXUBgNqvs',
-  authDomain: 'bounce-5c8eb.firebaseapp.com',
-  projectId: 'bounce-5c8eb',
-  storageBucket: 'bounce-5c8eb.appspot.com',
-  messagingSenderId: '381288365715',
-  appId: '1:381288365715:web:5845ebe542985cc4575766',
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
-
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const dbService = getFirestore(app);
 export const authService = getAuth(app);
 export const storage = getStorage(app);
